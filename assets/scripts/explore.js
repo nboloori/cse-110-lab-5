dbb8c80452649c9b5401cc002599227f123a6945
Navid Boloorian <navidboloorian0@gmail.com>
2022-10-24 15:30:46 -0700

    finished js section

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,72 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const speechSynthesis = window.speechSynthesis;
+
+  let voicesUpdated = false;
+
+  speechSynthesis.onvoiceschanged = () => {
+    voicesUpdated = true;
+    addVoices(speechSynthesis);
+  }
+
+  // backup if onvoiceschanged does not fire (seems to not do so when liveserver first starts)
+  setTimeout(() => {
+
+    if(!voicesUpdated) {
+      alert("HERE");
+      addVoices(speechSynthesis);
+    }
+  }, 500);
+
+  speakText(speechSynthesis);
+}
+
+function addVoices(speechSynthesis) {
+  const voices = speechSynthesis.getVoices();
+  const voiceSelect = document.getElementById("voice-select");
+
+  for(let i = 0; i < voices.length; i++) {
+    const voice = voices[i];
+    const voiceOption = document.createElement("option");
+
+    voiceOption.textContent = `${voice.name} (${voice.lang})`
+
+    if(voice.default) {
+      voiceOption.textContent += " - DEFAULT";
+    }
+
+    voiceOption.setAttribute("data-lang", voice.lang);
+    voiceOption.setAttribute("data-name", voice.name);
+    voiceSelect.append(voiceOption);
+  }
+}
+
+function speakText(speechSynthesis) {
+  const button = document.querySelector("#explore > button");
+
+  button.onclick = function () {
+    const text = document.getElementById("text-to-speak").value;
+    const utterance = new SpeechSynthesisUtterance(text);
+    const selectedVoice = document.getElementById("voice-select").selectedOptions[0].getAttribute("data-name");
+    const voices = speechSynthesis.getVoices();
+
+    for(let i = 0; i < voices.length; i++) {
+      if(voices[i].name == selectedVoice) {
+        utterance.voice = voices[i];
+      }
+    }
+
+    speechSynthesis.speak(utterance);
+
+    const faceImage = document.querySelector("#explore > img");
+
+    utterance.onstart = () => {
+      faceImage.src = "./assets/images/smiling-open.png";
+    }
+
+    utterance.onend = () => {
+      faceImage.src = "./assets/images/smiling.png";
+    }
+  }
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,71 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const jsConfetti = new JSConfetti();
+
+  setHorn();
+  setVolume();
+  playSound(jsConfetti);
+}
+
+function setHorn() {
+  const selected = document.getElementById("horn-select");
+
+  selected.onchange = () => {
+    const hornType = selected.value;
+    const altText = hornType.replace("-", " ");
+    const hornImage = document.querySelector("#expose > img");
+
+    hornImage.src = `assets/images/${hornType}.svg`;
+    hornImage.alt = altText;
+
+    const audioPlayer = document.querySelector("#expose > audio");
+
+    audioPlayer.src = `assets/audio/${hornType}.mp3`;
+  }
+}
+
+function setVolume() {
+  const volume = document.getElementById("volume");
+
+  volume.oninput = function () {
+    const volumeLevel = volume.value;
+    const volumeImage = document.querySelector("#volume-controls > img");
+
+    if(volumeLevel == 0) {
+      volumeImage.src = `assets/icons/volume-level-0.svg`;
+      volume.alt = "Volume level 0";
+    }
+    else if (volumeLevel >= 1 && volumeLevel < 33) {
+      volumeImage.src = `assets/icons/volume-level-1.svg`;
+      volume.alt = "Volume level 1";
+    }
+    else if (volumeLevel >= 33 && volumeLevel < 67) {
+      volumeImage.src = `assets/icons/volume-level-2.svg`;
+      volume.alt = "Volume level 2";
+    }
+    else {
+      volumeImage.src = `assets/icons/volume-level-3.svg`;
+      volume.alt = "Volume level 3";
+    }
+
+    const audioPlayer = document.querySelector("#expose > audio");
+
+    audioPlayer.volume = volumeLevel / 100;
+  }
+}
+
+function playSound(jsConfetti) {
+  const playButton = document.querySelector("#expose > button");
+  const audioPlayer = document.querySelector("#expose > audio");
+
+  playButton.onclick = function () {
+    audioPlayer.play();
+
+    const hornType = document.getElementById("horn-select").value;
+
+    if(hornType == "party-horn") {
+      jsConfetti.addConfetti();
+    }
+  };
 }
